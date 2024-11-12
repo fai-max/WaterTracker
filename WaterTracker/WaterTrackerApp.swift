f650d3ed9e5259bf5d1d5785fd857b412e182707
fammu03 <fammu03@gmail.com>
2024-11-12 23:11:56 +0530

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Fahim on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
